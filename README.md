eab72af48edc2d115c364be42cd8414dd2476a82
Dennis Lönard <dennis.loenard97@gmx.de>
2023-09-23 10:10:20 +0200

    rewritten as generators

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# hdf5-cli
-HDF5 file cli tool
+<div align="center">
+
+# HDF5 cli tool
+A python based cli tool to view HDF5 files
+
+![last commit](https://img.shields.io/github/last-commit/loenard97/hdf5-cli?&style=for-the-badge&logo=github&color=3776AB)
+![repo size](https://img.shields.io/github/repo-size/loenard97/hdf5-cli?&style=for-the-badge&logo=github&color=3776AB)
+
+</div>
+
+
+HDF5 Files are developed by the [HDF Group](https://www.hdfgroup.org/solutions/hdf5/).
+Each File can contain Groups that work similarly to folders and Datasets that represent raw data.
+They are widely used in Industry and Academia to store large sets of raw data.
+
+
+## 📋 Usage
+Specify a file name to print all Groups and Datasets in that file.
+The `-r` flag prints all Groups and Datasets recursively.
+Append one or more Datasets to print their contents.
+
+For example, to export Datasets to a csv file:
+
+```python
+$ python main.py data.h5 Data/x_data Data/y_data >out.csv
+```
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import h5py
 import argparse
 import sys
+
+import h5py
+import numpy as np
 
 
 def eprint(*args, **kwargs):
@@ -8,82 +10,79 @@ def eprint(*args, **kwargs):
     sys.exit(1)
 
 
-class RecursiveFileIterator:
+def read_h5(filename=None, dataset=None):
+    """Yield h5 dataset."""
+    with h5py.File(filename, "r") as file:
+        obj = file if dataset is None else file[dataset]
+        yield from np.array(obj)
 
-    def __init__(self, file: h5py.File):
-        self.file = file
-        self.pointer = "/"
 
-    def __str__(self):
-        return f"<{self.__class__.__name__}: {self.file.filename}, {self.pointer}>"
+def read_h5_recursive(file, dataset):
+    """Yield h5 dataset names recursively."""
+    obj = file if dataset is None else file[dataset]
 
-    def recursive_h5(self) -> list[str]:
-        """Iterate recursively through file."""
-        item_list = []
-        for name, obj in self.file.items():
-            if isinstance(obj, h5py.Group):
-                item_list.append(name)
-                for e in self.recursive_group(f"{name}"):
-                    item_list.append(e)
+    if isinstance(obj, h5py.Group):
+        for ds in obj:
+            next_dataset = ds if dataset is None else dataset + "/" + ds
+            yield from read_h5_recursive(file, next_dataset)
 
-            elif isinstance(obj, h5py.Dataset):
-                item_list.append(f"{name}")
+    elif isinstance(obj, h5py.Dataset):
+        yield obj.name
 
-        return item_list
 
-    def recursive_group(self, group: str) -> list[str]:
-        """Iterate recursively through group."""
-        item_list = []
-        for name, obj in self.file[group].items():
-            if isinstance(obj, h5py.Group):
-                item_list.append(f"{group}/{name}")
+def alternate_generators(*generators):
+    """Yield alternating generator."""
+    generator_list = [iter(g) for g in generators]
 
-                for e in self.recursive_group(f"{group}/{name}"):
-                    item_list.append(e)
+    while True:
+        try:
+            for g in generator_list:
+                yield next(g)
+        except StopIteration:
+            break
 
-            elif isinstance(obj, h5py.Dataset):
-                item_list.append(f"{group}/{name}")
+    for g in generator_list:
+        yield from g
 
-        return item_list
 
-    def __iter__(self):
-        for name in self.recursive_h5():
-            yield name
+def zip_generators(*generators):
+    """Yield zipped generator."""
+    generator_list = [iter(g) for g in generators]
+
+    while True:
+        try:
+            yield [next(g) for g in generator_list]
+        except StopIteration:
+            break
 
 
 def main():
     parser = argparse.ArgumentParser(
-        prog="hdf5-cli",
-        description="HDF5 file cli tool",
+        prog="H5 File Tool",
+        description="Cli tool to parse H5 file.",
     )
-    parser.add_argument("filename", nargs="?")
-    parser.add_argument("dataset", nargs="?")
-    parser.add_argument("-l", "--list", action="store_true", default=True)
+    parser.add_argument("filename")
+    parser.add_argument("dataset", nargs="*")
+    parser.add_argument("-d", "--delimiter", default=";")
     parser.add_argument("-r", "--recursive", action="store_true")
-
     args = parser.parse_args()
-    if not args.filename:
-        eprint("No file given")
 
-    with h5py.File(args.filename, "r") as file:
-        if args.dataset is not None:
-            obj = file[args.dataset]
-            if isinstance(obj, h5py.Group) or isinstance(obj, h5py.Dataset):
-                print(*obj, sep="\n")
-            else:
-                eprint("Type Error")
+    if args.recursive:
+        with h5py.File(args.filename, "r") as file:
+            print(*read_h5_recursive(file, None), sep="\n")
+        return
 
-        elif args.list and args.recursive:
-            print(*RecursiveFileIterator(file), sep="\n")
-
-        elif args.list:
-            print(*file, sep="\n")
-
-        else:
-            eprint("Unknown arguments")
+    if not args.dataset:
+        args.dataset = [None]
+    objs_generators = [read_h5(args.filename, ds) for ds in args.dataset]
+    zipped_generators = zip_generators(*objs_generators)
+    print(
+        *[args.delimiter.join(list(map(str, elems))) for elems in zipped_generators],
+        sep="\n"
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         main()
     except BaseException as err:
